86cacad09d95a195e8a54fbd39ef792a47b3472b
DNSGeek <tknox@mac.com>
2026-03-06 11:44:56 -0800

    Linting test

--- paravia.c
+++ paravia.c
@@ -19,7 +19,7 @@ as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
 */
 
- #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
